c254e6f9eb813626c09b7a8c42f5429ca7a2c683
Gray-Geist <gray.geistag@gmail.com>
2026-03-18 16:28:07 -0400

    Quartz sync: Mar 18, 2026, 4:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-		header: "Rajdhani",
-		body: "Share Tech",
-		code: "Space Mono",
+		header: "Orbitron",
+		body: "Exo 2",
+		code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
